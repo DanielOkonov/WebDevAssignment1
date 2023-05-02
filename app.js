59897fb62ddb34b08debfce1b2a4cb53b7339ec5
Daniel Okonov <daniel.okonov@gmail.com>
2023-05-01 21:02:31 -0700

    Final implementation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const session = require("express-session");
+const MongoDbSessionStore = require("connect-mongodb-session")(session);
 require("dotenv").config();
 
 const app = express();
@@ -14,6 +15,11 @@ const mongoConnectionUri = `mongodb+srv://${process.env.MONGODB_USER}:${process.
 const mongoDb = process.env.MONGODB_DATABASE;
 const mongoCollection = process.env.MONGODB_COLLECTION;
 
+var sessionStore = new MongoDbSessionStore({
+  uri: mongoConnectionUri,
+  collection: "sessions",
+});
+
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
@@ -24,8 +30,11 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   session({
-    secret: process.env.NODE_SESSION_SECRET,
-    //secret: 'secrettt',
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+    },
+    store: sessionStore,
     resave: false,
     saveUninitialized: false,
   })
@@ -79,31 +88,18 @@ router.post("/loginSubmit", async function (req, res) {
   }
 
   const client = new MongoClient(mongoConnectionUri);
-  //const client = new MongoClient('mongodb+srv://User230430:v8Ztsn35GZKJcgO7@cluster0.gwjgaoz.mongodb.net');
 
   await client.connect();
   console.log("Connected successfully to mongodb");
   const db = client.db(mongoDb);
-  //const db = client.db('asgn1');
   const usersCollection = db.collection(mongoCollection);
-  //const usersCollection = db.collection('users');
   const query = { _id: loginData.email };
   const userDocument = await usersCollection.findOne(query);
-  //console.log("userDocument: " + JSON.stringify(userDocument));
 
-  //ToDo: create pw hash below
-  // let hashedPassword;
-  // bcrypt.hash(loginData.password, saltRounds, function(err, hash) {
-  //   if(err){
-  //     console.log(err);
-  //   }
-  //   hashedPassword = hash;
-  // });
-
-  //-- hash doesn't work here
-  let hashedPassword = loginData.password;
-
-  if (!userDocument || userDocument.password !== hashedPassword) {
+  if (
+    !userDocument ||
+    !bcrypt.compareSync(loginData.password, userDocument.password)
+  ) {
     res.setHeader("Content-Type", "text/html");
     res.write("<p>Invalid email/password combination<p>");
     res.write('<a href="/login">Try again</a>');
@@ -111,9 +107,7 @@ router.post("/loginSubmit", async function (req, res) {
     return;
   }
 
-  //-- Create a session
   req.session.username = userDocument.username;
-  //--redirect the user to the /members page.
   res.redirect("/members");
 });
 
@@ -130,7 +124,6 @@ router.post("/signupSubmit", async function (req, res) {
     password: req.body.password,
   };
 
-  //--ToDo: using Joi to validate the input (name, email and password) so that NoSQL Injection attacks are not possible.
   const signupSchema = Joi.object({
     name: Joi.string().min(1).required(),
     email: Joi.string().min(3).required().email(),
@@ -147,47 +140,36 @@ router.post("/signupSubmit", async function (req, res) {
     return;
   }
 
-  //--ToDo: Add the name, email and a BCrypted hashed password as a user to the database.
-  // let hashedPassword;
-  // bcrypt.hash(signupData.password, saltRounds, function(err, hash) {
-  //   hashedPassword = hash;
-  // });
-
-  //-- hash works here but disabled cause it doesn't work in login
-  const hashedPassword = signupData.password;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hashedPassword = bcrypt.hashSync(signupData.password, salt);
 
   const client = new MongoClient(mongoConnectionUri);
+
+  await client.connect();
+  console.log("Connected successfully to mongodb");
+  const db = client.db(mongoDb);
+  const usersCollection = db.collection(mongoCollection);
+
   try {
-    await client.connect();
-    console.log("Connected successfully to mongodb");
-    const db = client.db(mongoDb);
-    const usersCollection = db.collection(mongoCollection);
-
-    try {
-      await usersCollection.insertOne({
-        _id: signupData.email,
-        username: signupData.name,
-        password: hashedPassword,
-      });
-    } catch (e) {
-      if (e.code === 11000) {
-        res.setHeader("Content-Type", "text/html");
-        res.write(`<p>User with email ${signupData.email} already exists<\p>`);
-        res.write('<a href="/signup">Try again</a>');
-        res.end();
-        return;
-      }
-      throw e;
+    await usersCollection.insertOne({
+      _id: signupData.email,
+      username: signupData.name,
+      password: hashedPassword,
+    });
+  } catch (e) {
+    if (e.code === 11000) {
+      res.setHeader("Content-Type", "text/html");
+      res.write(`<p>User with email ${signupData.email} already exists<\p>`);
+      res.write('<a href="/signup">Try again</a>');
+      res.end();
+      return;
     }
-
-    console.log("Signup data inserted to Db");
-  } finally {
-    await client.close();
+    throw e;
   }
 
-  //-- Create a session
+  console.log("Signup data inserted to Db");
+
   req.session.username = req.body.name;
-  //--redirect the user to the /members page.
   res.redirect("/members");
 });
 
@@ -197,7 +179,7 @@ router.get("/members", (req, res) => {
     return;
   }
   const imageFile = getRandomImageFile();
-  res.render("members", { userName: req.session.username });
+  res.render("members", { userName: req.session.username, image: imageFile });
 });
 
 router.get("/logout", (req, res) => {
@@ -205,25 +187,11 @@ router.get("/logout", (req, res) => {
   res.redirect("/");
 });
 
-//-- ToDo: test route, remove
-router.get("/setSession", (req, res) => {
-  req.session.username = "test-session-user-name";
-  res.end("session is set");
-});
-
-//-- ToDo: test route, remove
-router.get("/tryMongo", async function (req, res) {
-  const client = new MongoClient(mongoConnectionUri);
-  try {
-    await client.connect();
-    console.log("Connected successfully to server");
-    const db = client.db(mongoDb);
-    const usersCollection = db.collection(mongoCollection);
-    const cursor = usersCollection.find();
-    res.end("connected to MongoDb");
-  } finally {
-    await client.close();
-  }
+router.get("*", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.write("<p>Page not found<p>");
+  res.statusCode = 404;
+  res.end();
 });
 
 app.use("/", router);
@@ -232,6 +200,6 @@ app.listen(process.env.port || 3000);
 console.log("Running at Port 3000");
 
 function getRandomImageFile() {
-  //--ToDo: implement random
-  return "image_1.jpg";
+  const imageNum = 1 + Math.floor(Math.random() * 3);
+  return `image_${imageNum}.jpg`;
 }
